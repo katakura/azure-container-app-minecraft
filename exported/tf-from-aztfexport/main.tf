a99f743ce6c12ac1ed7d9bd3730d1526d71e290c
Yoshimasa Katakura <yoshimasa.katakura@outlook.com>
2024-10-21 14:45:03 +0900

    Refactor Terraform configuration for Minecraft server: parameterize resource group name and location, enforce TLS 1.2, and add monitoring diagnostics

--- exported/tf-from-aztfexport/main.tf
+++ exported/tf-from-aztfexport/main.tf
@@ -1,11 +1,22 @@
+variable "resource_group_name" {
+  default = "rg-minecraft-java-server"
+}
+variable "location" {
+  default = "japaneast"
+}
+
+resource "random_id" "storage_account" {
+  byte_length = 8
+}
+
 resource "azurerm_resource_group" "main" {
-  location = "japaneast"
-  name     = "rg-minecraft-java-server"
+  location = var.location
+  name     = var.resource_group_name
 }
 resource "azurerm_container_app" "mcjava" {
-  container_app_environment_id = "/subscriptions/SUBSCRIPTIONID/resourceGroups/rg-minecraft-java-server/providers/Microsoft.App/managedEnvironments/acaenv-minecraft"
+  container_app_environment_id = azurerm_container_app_environment.main.id
   name                         = "minecraft"
-  resource_group_name          = "rg-minecraft-java-server"
+  resource_group_name          = azurerm_resource_group.main.name
   revision_mode                = "Single"
   ingress {
     exposed_port     = 25565
@@ -59,56 +70,39 @@ resource "azurerm_container_app" "mcjava" {
       storage_type = "AzureFile"
     }
   }
-  depends_on = [
-    azurerm_container_app_environment.main,
-  ]
 }
 resource "azurerm_container_app_environment" "main" {
-  infrastructure_subnet_id = "/subscriptions/SUBSCRIPTIONID/resourceGroups/rg-minecraft-java-server/providers/Microsoft.Network/virtualNetworks/vnet-minecraft/subnets/snet-aca"
-  location                 = "japaneast"
-  name                     = "acaenv-minecraft"
-  resource_group_name      = "rg-minecraft-java-server"
-  depends_on = [
-    azurerm_subnet.aca,
-  ]
+  infrastructure_subnet_id   = azurerm_subnet.aca.id
+  location                   = azurerm_resource_group.main.location
+  name                       = "acaenv-minecraft"
+  resource_group_name        = azurerm_resource_group.main.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
 }
 resource "azurerm_container_app_environment_storage" "main" {
-  access_key                   = ""
+  access_key                   = azurerm_storage_account.main.primary_access_key
   access_mode                  = "ReadWrite"
-  account_name                 = "stminecraftz6g4sa4sns7fw"
-  container_app_environment_id = "/subscriptions/SUBSCRIPTIONID/resourceGroups/rg-minecraft-java-server/providers/Microsoft.App/managedEnvironments/acaenv-minecraft"
+  account_name                 = azurerm_storage_account.main.name
+  container_app_environment_id = azurerm_container_app_environment.main.id
   name                         = "mcdata"
   share_name                   = "mcdata"
-  depends_on = [
-    azurerm_container_app_environment.main,
-  ]
 }
 resource "azurerm_virtual_network" "main" {
   address_space       = ["10.10.0.0/16"]
-  location            = "japaneast"
+  location            = azurerm_resource_group.main.location
   name                = "vnet-minecraft"
-  resource_group_name = "rg-minecraft-java-server"
-  depends_on = [
-    azurerm_resource_group.main,
-  ]
+  resource_group_name = azurerm_resource_group.main.name
 }
 resource "azurerm_subnet" "aca" {
   address_prefixes     = ["10.10.0.0/23"]
   name                 = "snet-aca"
-  resource_group_name  = "rg-minecraft-java-server"
+  resource_group_name  = azurerm_resource_group.main.name
   service_endpoints    = ["Microsoft.Storage"]
-  virtual_network_name = "vnet-minecraft"
-  depends_on = [
-    azurerm_virtual_network.main,
-  ]
+  virtual_network_name = azurerm_virtual_network.main.name
 }
 resource "azurerm_log_analytics_workspace" "main" {
-  location            = "japaneast"
+  location            = azurerm_resource_group.main.location
   name                = "log-minecraft"
-  resource_group_name = "rg-minecraft-java-server"
-  depends_on = [
-    azurerm_resource_group.main,
-  ]
+  resource_group_name = azurerm_resource_group.main.name
 }
 resource "azurerm_storage_account" "main" {
   account_kind                     = "FileStorage"
@@ -116,16 +110,21 @@ resource "azurerm_storage_account" "main" {
   account_tier                     = "Premium"
   allow_nested_items_to_be_public  = false
   cross_tenant_replication_enabled = false
-  location                         = "japaneast"
-  min_tls_version                  = "TLS1_0"
-  name                             = "stminecraftz6g4sa4sns7fw"
-  resource_group_name              = "rg-minecraft-java-server"
-  depends_on = [
-    azurerm_resource_group.main,
-  ]
+  location                         = azurerm_resource_group.main.location
+  min_tls_version                  = "TLS1_2"
+  name                             = "stmc${lower(random_id.storage_account.hex)}"
+  resource_group_name              = azurerm_resource_group.main.name
 }
 resource "azurerm_storage_share" "mcdata" {
   name                 = "mcdata"
   quota                = 100
-  storage_account_name = "stminecraftz6g4sa4sns7fw"
+  storage_account_name = azurerm_storage_account.main.name
+}
+resource "azurerm_monitor_diagnostic_setting" "main" {
+  name                       = "log-minecraft"
+  target_resource_id         = azurerm_container_app_environment.main.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+  enabled_log {
+    category_group = "allLogs"
+  }
 }
